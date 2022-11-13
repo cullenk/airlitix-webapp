1d1695b4afdea8d104085aaee4d8da7155356626
Airlitix <johnk@airlitix.com>
2022-11-13 04:49:52 -0500

    fontSize change

--- views/js/commonV3.js
+++ views/js/commonV3.js
@@ -81,10 +81,18 @@ $(document).ready(() => {
 
     let officeName = $(".office-div > h3").html();
 
-    document.getElementsByClassName('panel-text-r1').style.fontSize="38px";
-    document.getElementsByClassName('panel-text-r2').style.fontSize="38px";
-    document.getElementsByClassName('panel-text-r3').style.fontSize="38px";
-    document.getElementsByClassName('panel-text-r4').style.fontSize="38px";
+    // document.getElementsByClassName('panel-text-r1').style.fontSize="38px";
+    // document.getElementsByClassName('panel-text-r2').style.fontSize="38px";
+    // document.getElementsByClassName('panel-text-r3').style.fontSize="38px";
+    // document.getElementsByClassName('panel-text-r4').style.fontSize="38px";
+    let thing = document.getElementsByClassName('panel-text-r1');
+    thing.firstElementChild.style.fontSize = '38px';
+     thing = document.getElementsByClassName('panel-text-r2');
+    thing.firstElementChild.style.fontSize = '38px';
+     thing = document.getElementsByClassName('panel-text-r3');
+    thing.firstElementChild.style.fontSize = '38px';
+     thing = document.getElementsByClassName('panel-text-r4');
+    thing.firstElementChild.style.fontSize = '38px';
 
     socket.on('WIFI-CLIENT-CONNECTED', (data) => {
       if (data.socketId) {
